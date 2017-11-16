6fffdce4a6a2b14b977fa9413567a641229a1f00
dchenam <dnnschen39@gmail.com>
2017-11-16 10:27:43 +0800

    added masking
    Former-commit-id: 67ac5301a3c0974847f5216c9a9975cbf5cb21fa

--- hw2/preprocess.py
+++ hw2/preprocess.py
@@ -23,49 +23,50 @@ def load_data(data_dir='MLDS_hw2_data/', type='train', vocab_size=3000):
         feat_vec[i] = feat[1]
 
     # Normalize Input Vector
-    mean = np.mean(feat_vec)
-    std = np.std(feat_vec)
-    normalized_feat = (feat_vec - mean) / std
+    #mean = np.mean(feat_vec)
+    #std = np.std(feat_vec)
+    #normalized_feat = (feat_vec - mean) / std
+    normalized_feat = feat_vec
 
-    if type == 'train':
-        with open(label_path) as data_file:
-            label = json.load(data_file)
+    #if type == 'train':
+    with open(label_path) as data_file:
+        label = json.load(data_file)
 
-        sorted_label = sorted(label, key=lambda k: k['id'])
-        for label in sorted_label:
-            label['caption'] = list(map(lambda x: x + '<eos>', label['caption']))
+    sorted_label = sorted(label, key=lambda k: k['id'])
+    for label in sorted_label:
+        label['caption'] = list(map(lambda x: x + '<eos>', label['caption']))
 
-        all_captions = ['<bos>' + item for label in sorted_label for item in label['caption']]
+    all_captions = ['<bos>' + item for label in sorted_label for item in label['caption']]
 
-        tokenizer = Tokenizer(num_words=vocab_size)
-        tokenizer.fit_on_texts(all_captions)
-        tokenized_captions = [tokenizer.texts_to_sequences(label['caption']) for label in sorted_label]
+    tokenizer = Tokenizer(num_words=vocab_size)
+    tokenizer.fit_on_texts(all_captions)
+    tokenized_captions = [tokenizer.texts_to_sequences(label['caption']) for label in sorted_label]
 
-        tokenizer.word_index['pad'] = 0
+    tokenizer.word_index['pad'] = 0
 
-        idx_to_word = {v: k for k, v in tokenizer.word_index.items()}
+    idx_to_word = {v: k for k, v in tokenizer.word_index.items()}
 
-        with open('word_to_idx.pickle', 'wb') as handle:
-            pickle.dump(tokenizer.word_index, handle)
+    with open('word_to_idx.pickle', 'wb') as handle:
+        pickle.dump(tokenizer.word_index, handle)
 
-        with open('idx_to_word.pickle', 'wb') as handle:
-            pickle.dump(idx_to_word, handle)
+    with open('idx_to_word.pickle', 'wb') as handle:
+        pickle.dump(idx_to_word, handle)
 
-        padded_captions = [pad_sequences(caption, padding='post', maxlen=44) for caption in tokenized_captions]
+    padded_captions = [pad_sequences(caption, padding='post', maxlen=44) for caption in tokenized_captions]
 
-        np.save('y_' + type + '.npy', padded_captions)
+    np.save('y_' + type + '.npy', padded_captions)
     np.save('idx_' + type + '.npy', idx)
     np.save('x_' + type + '.npy', normalized_feat)
 
 
 if __name__ == "__main__":
-    import sys
+
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_dir', default='./MLDS_hw2_data/')
-    parser.add_argument('--train_or_test', default='train')
+    parser.add_argument('-mode', default='train')
 
     args = parser.parse_args()
     print(args)
 
-    load_data(data_dir=args.data_dir, type=args.train_or_test)
+    load_data(data_dir=args.data_dir, type=args.mode)
