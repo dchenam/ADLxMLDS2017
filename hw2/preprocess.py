96c98d132cb3e1736d24b0267ce1255897c483c2
dchenam <dnnschen39@gmail.com>
2017-11-16 11:00:25 +0800

    included test data in preprocess

--- hw2/preprocess.py
+++ hw2/preprocess.py
@@ -1,18 +1,11 @@
-
-def load_data(data_dir='MLDS_hw2_data/', type='train', vocab_size=3000):
-    import numpy as np
-    import json
+def get_feat(data_dir='MLDS_hw2_data/', type='train'):
     import glob
     import os
-    import pickle
-    from keras.preprocessing.text import Tokenizer, one_hot
-    from keras.preprocessing.sequence import pad_sequences
-
+    import numpy as np
     feat_path = os.path.join(data_dir, type + 'ing_data/feat/')
-    label_path = os.path.join(data_dir, type+ 'ing_label.json')
 
     feat = {}
-    for name in glob.glob(os.path.join(feat_path,'*.npy')):
+    for name in glob.glob(os.path.join(feat_path, '*.npy')):
         feat[os.path.splitext(os.path.basename(name))[0]] = np.load(name)
 
     sorted_feat = sorted(feat.items())
@@ -23,38 +16,61 @@ def load_data(data_dir='MLDS_hw2_data/', type='train', vocab_size=3000):
         feat_vec[i] = feat[1]
 
     # Normalize Input Vector
-    #mean = np.mean(feat_vec)
-    #std = np.std(feat_vec)
-    #normalized_feat = (feat_vec - mean) / std
-    normalized_feat = feat_vec
+    # mean = np.mean(feat_vec)
+    # std = np.std(feat_vec)
+    # normalized_feat = (feat_vec - mean) / std
 
-    #if type == 'train':
+    normalized_feat = feat_vec
+    return feat_vec, idx
+
+def get_label(data_dir='MLDS_hw2_data/', type='train'):
+    import os
+    import json
+    label_path = os.path.join(data_dir, type + 'ing_label.json')
     with open(label_path) as data_file:
         label = json.load(data_file)
 
     sorted_label = sorted(label, key=lambda k: k['id'])
     for label in sorted_label:
         label['caption'] = list(map(lambda x: x + '<eos>', label['caption']))
+    return sorted_label
 
-    all_captions = ['<bos>' + item for label in sorted_label for item in label['caption']]
+def load_data(data_dir='MLDS_hw2_data/', type='train', include='train_data', vocab_size=3000):
+    import numpy as np
+    import pickle
+    from keras.preprocessing.text import Tokenizer, one_hot
+    from keras.preprocessing.sequence import pad_sequences
 
-    tokenizer = Tokenizer(num_words=vocab_size)
-    tokenizer.fit_on_texts(all_captions)
-    tokenized_captions = [tokenizer.texts_to_sequences(label['caption']) for label in sorted_label]
+    if type == 'train':
+        normalized_feat, idx = get_feat(data_dir, 'train')
+        sorted_label = get_label(data_dir, 'train')
+        all_captions = ['<bos>' + item for label in sorted_label for item in label['caption']]
 
-    tokenizer.word_index['pad'] = 0
+        tokenizer = Tokenizer(num_words=vocab_size)
+        tokenizer.fit_on_texts(all_captions)
+        tokenized_captions = [tokenizer.texts_to_sequences(label['caption']) for label in sorted_label]
+        tokenizer.word_index['pad'] = 0
+        idx_to_word = {v: k for k, v in tokenizer.word_index.items()}
 
-    idx_to_word = {v: k for k, v in tokenizer.word_index.items()}
+        with open('word_to_idx.pickle', 'wb') as handle:
+            pickle.dump(tokenizer.word_index, handle)
+        with open('idx_to_word.pickle', 'wb') as handle:
+            pickle.dump(idx_to_word, handle)
 
-    with open('word_to_idx.pickle', 'wb') as handle:
-        pickle.dump(tokenizer.word_index, handle)
+        padded_captions = [pad_sequences(caption, padding='post', maxlen=44) for caption in tokenized_captions]
+        np.save('y_' + type + '.npy', padded_captions)
+        np.save('idx_' + type + '.npy', idx)
+        np.save('x_' + type + '.npy', normalized_feat)
 
-    with open('idx_to_word.pickle', 'wb') as handle:
-        pickle.dump(idx_to_word, handle)
+        if include == 'test_data':
+            normalized_feat, idx = get_feat(data_dir, 'test')
+            sorted_label = get_label(data_dir, 'test')
+            tokenized_captions = [tokenizer.texts_to_sequences(label['caption']) for label in sorted_label]
+            padded_captions = [pad_sequences(caption, padding='post', maxlen=44) for caption in tokenized_captions]
+            np.save('y_test.npy', padded_captions)
+            np.save('idx_test.npy', idx)
+            np.save('x_test.npy', normalized_feat)
 
-    padded_captions = [pad_sequences(caption, padding='post', maxlen=44) for caption in tokenized_captions]
-
-    np.save('y_' + type + '.npy', padded_captions)
     np.save('idx_' + type + '.npy', idx)
     np.save('x_' + type + '.npy', normalized_feat)
 
@@ -65,8 +81,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_dir', default='./MLDS_hw2_data/')
     parser.add_argument('-mode', default='train')
+    parser.add_argument('-include', default='train_data')
 
     args = parser.parse_args()
     print(args)
 
-    load_data(data_dir=args.data_dir, type=args.mode)
+    load_data(data_dir=args.data_dir, type=args.mode, include=args.include)
