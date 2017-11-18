de0ff6378428c4e14886e970df01892788e58366
dchenam <dnnschen39@gmail.com>
2017-11-18 21:14:48 +0800

    added weight loading

--- hw2/seq2seq.py
+++ hw2/seq2seq.py
@@ -6,12 +6,12 @@ def seq2seq_model(max_length=3000, padded_length=44, hidden_units=256):
     encoder_states = [state_h, state_c]
 
     decoder_input = Input(shape=(padded_length, max_length), name='decoder_input')
-    masked_input = Masking()(decoder_input)
-    repeat_input = RepeatVector(44, name='repeat_encoder_output')(encoder_output)
-    concated_input = Concatenate(axis=-1, name='concated_input')([masked_input, repeat_input])
+#    masked_input = Masking()(decoder_input)
+#    repeat_input = RepeatVector(44, name='repeat_encoder_output')(encoder_output)
+#    concated_input = Concatenate(axis=-1, name='concated_input')([masked_input, repeat_input])
 
     decoder_lstm = LSTM(hidden_units, return_sequences=True, return_state=True, name='decoder_lstm', implementation=2)
-    decoder_output, _, _ = decoder_lstm(concated_input, initial_state=encoder_states)
+    decoder_output, _, _ = decoder_lstm(decoder_input, initial_state=encoder_states)
     decoder_dense = Dense(max_length, activation='softmax', name='decoder_dense')
     decoder_output = decoder_dense(decoder_output)
 
@@ -25,21 +25,19 @@ def seq2seq_model(max_length=3000, padded_length=44, hidden_units=256):
     decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
 
     decoder_output, state_h, state_c = decoder_lstm(
-        concated_input, initial_state=decoder_states_inputs)
+        decoder_input, initial_state=decoder_states_inputs)
     decoder_states = [state_h, state_c]
     decoder_output = decoder_dense(decoder_output)
     decoder_model = Model(
-        [decoder_input, encoder_input] + decoder_states_inputs,
+        [decoder_input] + decoder_states_inputs,
         [decoder_output] + decoder_states)
-
+    print(encoder_model, decoder_model)
     return model, encoder_model, decoder_model
 
 def train(model, data, max_length=3000, padded_length=44, batch_size=32, epochs=200):
-
-    seq2seq_model, encoder_model, decoder_model = model
     x_train, y_train, y_train_shift, x_test, y_test, y_test_shift = data
 
-    seq2seq_model.compile(optimizer="Adam", loss="categorical_crossentropy", metrics=['accuracy'])
+    model.compile(optimizer="Adam", loss="categorical_crossentropy", metrics=['accuracy'])
 
     def data_generator(features, labels, shifted_labels, batch_size):
         batch_encoder_input = np.zeros((batch_size, features.shape[1], features.shape[2]))
@@ -62,28 +60,27 @@ def train(model, data, max_length=3000, padded_length=44, batch_size=32, epochs=
 
             yield [batch_encoder_input, batch_decoder_input], batch_decoder_output
 
-    checkpointer = ModelCheckpoint(filepath='/models/best.h5', verbose=1, save_best_only=True)
+    checkpoint = ModelCheckpoint(filepath='/models/best.h5', verbose=1, save_best_only=True)
 
     import time
     start_time = time.time()
 
-    seq2seq_model.fit_generator(
+    model.fit_generator(
         data_generator(x_train, y_train, y_train_shift, batch_size=batch_size),
         steps_per_epoch=len(x_train) / batch_size,
         epochs=epochs,
-        callbacks=[checkpointer],
+        callbacks=[checkpoint],
         verbose=1)
     end_time = time.time()
     seconds = (end_time - start_time)
 
     print(str(seconds / 60) + " minutes")
 
-    seq2seq_model.save('models/seq2seq.h5')
-    encoder_model.save('models/encoder.h5')
-    decoder_model.save('models/decoder.h5')
-    return seq2seq_model
+    model.save_weights('models/seq2seq.h5', by_name=True)
 
-def decode_sequence(input_seq, wordtoidx, idxtoword, encoder_model, decoder_model, padded_length=44, max_length=3000):
+    return model
+
+def decode_sequence(input_seq, encoder_model, decoder_model, wordtoidx, idxtoword, padded_length=44, max_length=3000):
     # Encode the input as state vectors.
     states_value = encoder_model.predict(input_seq)
 
@@ -99,12 +96,12 @@ def decode_sequence(input_seq, wordtoidx, idxtoword, encoder_model, decoder_mode
     formatted_sentence = ''
     while not stop_condition:
         output_tokens, h, c = decoder_model.predict(
-            [target_seq, input_seq] + states_value)
+            [target_seq] + states_value)
         # Sample a token
         sampled_token_index = np.argmax(output_tokens[0, -1, :])
         sampled_char = idxtoword[sampled_token_index]
 
-        if sampled_char != 'eos':
+        if sampled_char != 'eos' and sampled_char != 'pad':
             formatted_sentence += sampled_char + ' '
         decoded_sentence += sampled_char
         # Exit condition: either hit max length
@@ -113,17 +110,16 @@ def decode_sequence(input_seq, wordtoidx, idxtoword, encoder_model, decoder_mode
             stop_condition = True
 
         print(sampled_token_index)
+
         # Update the target sequence (of length 1).
         target_seq = np.zeros((1, padded_length, max_length))
         target_seq[0, 0, sampled_token_index] = 1.
 
         # Update states
         states_value = [h, c]
-
     return formatted_sentence
 
-def test(model):
-
+def test(encoder_model, decoder_model):
     x_test = np.load('x_test.npy')
     idx_test = np.load('idx_test.npy')
 
@@ -133,48 +129,43 @@ def test(model):
         idxtoword = pickle.load(handle)
 
     result = {}
-
+    print(encoder_model)
+    print(decoder_model)
     for i in range(len(x_test)):
-        result[idx_test[i]] = decode_sequence(x_test[i].reshape((1, 80, 4096)), wordtoidx, idxtoword, model)
+        result[idx_test[i]] = decode_sequence(x_test[i].reshape((1, 80, 4096)), encoder_model, decoder_model, wordtoidx, idxtoword)
 
     return result
 
 if __name__ == "__main__":
-
     import os
     import h5py
-    from keras.models import load_model
+    from keras.models import Model, load_model
     import numpy as np
     import pickle
-    from keras.models import Model
     from keras.layers import Input, LSTM, Dense, Masking, RepeatVector, Concatenate, Lambda
     from keras.utils import to_categorical
     from keras.callbacks import ModelCheckpoint
-    from keras.preprocessing.sequence import pad_sequences
+    import keras.backend as K
 
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('-mode', default='test', type=str)
     parser.add_argument('--save_dir', default='./models')
     parser.add_argument('--output_dir', default='./result.txt')
+    parser.add_argument('-batch_size', default=32)
     args = parser.parse_args()
     print(args)
     if not os.path.exists(args.save_dir):
         os.makedirs(args.save_dir)
 
     seq2seq_model, encoder_model, decoder_model = seq2seq_model()
-
     seq2seq_model.summary()
-
 
     if args.mode == 'test':
         import pandas as pd
-
-        model = load_model('models/seq2seq.h5')
-        encoder_model = load_model('models/encoder.h5')
-        decoder_model = load_model('models/decoder.h5')
-
-        predicted = test(model)
+        encoder_model.load_weights('models/seq2seq.h5', by_name=True)
+        decoder_model.load_weights('models/seq2seq.h5', by_name=True)
+        predicted = test(encoder_model, decoder_model)
         output = pd.DataFrame.from_dict(predicted, orient='index').to_csv(args.output_dir, header=None)
     else:
         x_train = np.load('x_train.npy')
@@ -186,4 +177,4 @@ if __name__ == "__main__":
         data = [x_train, y_train, y_train_shift, x_test, y_test, y_test_shift]
 
         model = [seq2seq_model, encoder_model, decoder_model]
-        train(model, data)
+        train(seq2seq_model, data, batch_size=int(args.batch_size))
