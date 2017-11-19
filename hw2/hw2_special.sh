6cf9554f0f783581723f39de407f64892f21a384
dchenam <dnnschen39@gmail.com>
2017-11-19 23:49:54 +0800

    final commit
    Former-commit-id: ceb166a6543711d382680d72003cefd7100ea145

--- hw2/hw2_special.sh
+++ hw2/hw2_special.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 data_path="$1"
 output_path="$2"
-wget 'https://www.dropbox.com/s/08y4rkzn4ityeae/model2.h5?dl=1' -O model2.h5
-python preprocess.py --data_dir $data_path --train_or_test 'test'
-python seq2seq.py --output_dir $output_path
+wget 'https://www.dropbox.com/s/08y4rkzn4ityeae/model2.h5?dl=1' -O model.h5
+python preprocess.py --data_dir $data_path -mode 'test'
+python seq2seq.py --output_dir $output_path --model_dir './model.h5'
 
 -
--- hw2/preprocess.py
+++ hw2/preprocess.py
@@ -7,9 +7,7 @@ def get_feat(data_dir='MLDS_hw2_data/', type='train'):
     feat_dict = {}
     for name in glob.glob(os.path.join(feat_path, '*.npy')):
         feat_dict[os.path.splitext(os.path.basename(name))[0]] = np.load(name)
-
     return feat_dict
-
 
 def get_label(data_dir='MLDS_hw2_data/', type='train'):
     import os
@@ -22,8 +20,9 @@ def get_label(data_dir='MLDS_hw2_data/', type='train'):
         label['caption'] = list(map(lambda x: x + '<eos>', label['caption']))
     return label_dict
 
-def get_data(data_dir='MLDS_hw2_data/', type='train'):
+def get_data(data_dir='MLDS_hw2_data/', type='train', id_list='none'):
     import numpy as np
+    import os
     feat_dict = get_feat(data_dir, type)
     label_dict = get_label(data_dir, type)
     idx = [None] * len(feat_dict)
@@ -34,15 +33,32 @@ def get_data(data_dir='MLDS_hw2_data/', type='train'):
         key = label_dict[i]['id']
         sorted_feat[i] = feat_dict[key]
         idx[i] = key
-    #Normalize Data
-    mean = np.mean(sorted_feat)
-    std = np.std(sorted_feat)
-    feat_data = (sorted_feat - mean) / std
+        if id_list != 'none':
+            id_path = os.path.join(data_dir, id_list)
+            result = []
+            with open(id_path, 'r') as f:
+                result = f.readlines()
+                for i, line in enumerate(result):
+                    result[i] = line.strip('\n')
+            selected_idx = [None] * len(result)
+            selected_feat = [None] * len(result)
+            selected_label = [None] * len(result)
+            for i, j in enumerate(result):
+                selected_feat[i] = sorted_feat[idx.index(j)]
+                selected_label[i] = sorted_label[idx.index(j)]
+                selected_idx[i] = j
+            # Normalize Data
+            mean = np.mean(selected_feat)
+            std = np.std(selected_feat)
+            selected_feat = (sorted_feat - mean) / std
+            return selected_idx, selected_feat, selected_label
+        # Normalize Data
+        mean = np.mean(sorted_feat)
+        std = np.std(sorted_feat)
+        sorted_feat = (sorted_feat - mean) / std
+    return idx, sorted_feat, sorted_label
 
-    return idx, feat_data, sorted_label
-
-
-def load_data(data_dir='MLDS_hw2_data/', type='train', include='train_data', vocab_size=3000):
+def load_data(data_dir='MLDS_hw2_data/', type='train', include='train_data', id_list='none', vocab_size=3000):
     import numpy as np
     import pickle
     from keras.preprocessing.text import Tokenizer
@@ -86,7 +102,7 @@ def load_data(data_dir='MLDS_hw2_data/', type='train', include='train_data', voc
             np.save('idx_test.npy', idx)
             np.save('x_test.npy', normalized_feat)
     else:
-        idx, normalized_feat, sorted_label = get_data(data_dir, 'test')
+        idx, normalized_feat, sorted_label = get_data(data_dir, 'test', id_list)
         np.save('idx_' + type + '.npy', idx)
         np.save('x_' + type + '.npy', normalized_feat)
 
@@ -98,8 +114,8 @@ if __name__ == "__main__":
     parser.add_argument('--data_dir', default='./MLDS_hw2_data/')
     parser.add_argument('-mode', default='train')
     parser.add_argument('-include', default='train_data')
-
+    parser.add_argument('-use_list', default='none')
     args = parser.parse_args()
     print(args)
 
-    load_data(data_dir=args.data_dir, type=args.mode, include=args.include)
+    load_data(data_dir=args.data_dir, type=args.mode, include=args.include, id_list=args.use_list)
