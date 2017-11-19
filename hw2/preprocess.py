cb2b31d4ed802ec30ddcb5bc2cd7b4b5da21129d
dchenam <dnnschen39@gmail.com>
2017-11-20 05:17:38 +0800

    fixed peer review bug
    Former-commit-id: 400932f2b230c04a667ea192e7c21e44e38cfb2c

--- hw2/preprocess.py
+++ hw2/preprocess.py
@@ -1,121 +1,107 @@
-def get_feat(data_dir='MLDS_hw2_data/', type='train'):
+def get_feat(data_dir='MLDS_hw2_data/', mode ='training_data'):
     import glob
     import os
     import numpy as np
-    feat_path = os.path.join(data_dir, type + 'ing_data/feat/')
-
+    feat_path = os.path.join(data_dir, mode +'/feat/')
     feat_dict = {}
     for name in glob.glob(os.path.join(feat_path, '*.npy')):
         feat_dict[os.path.splitext(os.path.basename(name))[0]] = np.load(name)
     return feat_dict
 
-def get_label(data_dir='MLDS_hw2_data/', type='train'):
+def get_label(data_dir='MLDS_hw2_data/', mode='training_label'):
     import os
     import json
-
-    label_path = os.path.join(data_dir, type + 'ing_label.json')
+    label_path = os.path.join(data_dir, mode + '.json')
     with open(label_path) as data_file:
         label_dict = json.load(data_file)
-    for label in label_dict:
-        label['caption'] = list(map(lambda x: x + '<eos>', label['caption']))
     return label_dict
 
-def get_data(data_dir='MLDS_hw2_data/', type='train', id_list='none'):
+def normalize_feat(feat_data):
+    import numpy as np
+    mean = np.mean(feat_data)
+    std = np.std(feat_data)
+    return (feat_data - mean) / std
+
+def dict_to_data(feat_dict, label_dict):
+    video_id = [None] * len(feat_dict)
+    feat_data = [None] * len(feat_dict)
+    label_data = [None] * len(feat_dict)
+    for label in label_dict:
+        label['caption'] = list(map(lambda x: x + '<eos>', label['caption']))
+    for i in range(len(feat_dict)):
+        label_data[i] = label_dict[i]['caption']
+        key = label_dict[i]['id']
+        feat_data[i] = feat_dict[key]
+        video_id[i] = key
+    return video_id, feat_data, label_data
+
+def process_data(data_dir='MLDS_hw2_data/', mode='train'):
     import numpy as np
     import os
-    feat_dict = get_feat(data_dir, type)
-    label_dict = get_label(data_dir, type)
-    idx = [None] * len(feat_dict)
-    sorted_feat = [None] * len(feat_dict)
-    sorted_label = [None] * len(label_dict)
-    for i in range(len(feat_dict)):
-        sorted_label[i] = label_dict[i]['caption']
-        key = label_dict[i]['id']
-        sorted_feat[i] = feat_dict[key]
-        idx[i] = key
-        if id_list != 'none':
-            id_path = os.path.join(data_dir, id_list)
-            result = []
-            with open(id_path, 'r') as f:
-                result = f.readlines()
-                for i, line in enumerate(result):
-                    result[i] = line.strip('\n')
-            selected_idx = [None] * len(result)
-            selected_feat = [None] * len(result)
-            selected_label = [None] * len(result)
-            for i, j in enumerate(result):
-                selected_feat[i] = sorted_feat[idx.index(j)]
-                selected_label[i] = sorted_label[idx.index(j)]
-                selected_idx[i] = j
-            # Normalize Data
-            mean = np.mean(selected_feat)
-            std = np.std(selected_feat)
-            selected_feat = (sorted_feat - mean) / std
-            return selected_idx, selected_feat, selected_label
-        # Normalize Data
-        mean = np.mean(sorted_feat)
-        std = np.std(sorted_feat)
-        sorted_feat = (sorted_feat - mean) / std
-    return idx, sorted_feat, sorted_label
+    if mode == 'train':
+        feat_dict = get_feat(data_dir, 'training_data')
+        label_dict = get_label(data_dir, 'training_label')
+        video_id, feat_data, label_data = dict_to_data(feat_dict, label_dict)
+        normalize_feat(feat_data)
+    elif mode == 'test':
+        feat_dict = get_feat(data_dir, 'testing_data')
+        label_dict = get_label(data_dir, 'testing_label')
+        video_id, feat_data, label_data = dict_to_data(feat_dict, label_dict)
+        normalize_feat(feat_data)
+    elif mode == 'peer_review':
+        feat_dict = get_feat(data_dir, 'peer_review')
+        id_list_path = os.path.join(data_dir, 'peer_review_id.txt')
+        id_list = []
+        video_id = [None] * len(feat_dict)
+        feat_data = [None] * len(feat_dict)
+        with open(id_list_path, 'r') as f:
+            id_list = f.readlines()
+        for i, j in enumerate(id_list):
+            key = j.strip('\n')
+            video_id[i] = key
+            feat_data[i] = feat_dict[key]
+        normalize_feat(feat_data)
+        return video_id, feat_data
+    return video_id, feat_data, label_data
 
-def load_data(data_dir='MLDS_hw2_data/', type='train', include='train_data', id_list='none', vocab_size=3000):
+
+def load_data(data_dir='MLDS_hw2_data/', mode='train', include='test', vocab_size=3000):
     import numpy as np
     import pickle
     from keras.preprocessing.text import Tokenizer
-    from keras.preprocessing.sequence import pad_sequences
 
-    if type == 'train':
-        idx, normalized_feat, sorted_label= get_data(data_dir, 'train')
-
-        all_captions = ['<bos>' + item for label in sorted_label for item in label]
+    if mode == 'train':
+        video_id, feat_data, label_data = process_data(mode='train')
+        flattened_labels = ['<bos>' + item for label in label_data for item in label]
         tokenizer = Tokenizer(num_words=vocab_size)
-        tokenizer.fit_on_texts(all_captions)
-        tokenized_captions = [tokenizer.texts_to_sequences(label) for label in sorted_label]
+        tokenizer.fit_on_texts(flattened_labels)
         tokenizer.word_index['pad'] = 0
-        idx_to_word = {v: k for k, v in tokenizer.word_index.items()}
+        label_data = [tokenizer.texts_to_sequences(label) for label in label_data]
 
-        with open('word_to_idx.pickle', 'wb') as handle:
+        with open('tokenizer.pickle', 'wb') as handle:
             pickle.dump(tokenizer.word_index, handle)
-        with open('idx_to_word.pickle', 'wb') as handle:
-            pickle.dump(idx_to_word, handle)
-
-        shifted_tokenized_captions = [[[2] + sequence for sequence in captions] for captions in tokenized_captions]
-        padded_shift = [pad_sequences(caption, padding='post', maxlen=44) for caption in shifted_tokenized_captions]
-        padded_captions = [pad_sequences(caption, padding='post', maxlen=44) for caption in tokenized_captions]
-
-        np.save('y_shift_' + type + '.npy', padded_shift)
-        np.save('y_' + type + '.npy', padded_captions)
-        np.save('idx_' + type + '.npy', idx)
-        np.save('x_' + type + '.npy', normalized_feat)
-
-        if include == 'test_data':
-            idx, normalized_feat, sorted_label = get_data(data_dir, 'test')
-
-            tokenized_captions = [tokenizer.texts_to_sequences(label) for label in sorted_label]
-            shifted_tokenized_captions = [[[2] + sequence for sequence in captions] for captions in
-                                          tokenized_captions]
-            padded_shift = [pad_sequences(caption, padding='post', maxlen=44) for caption in shifted_tokenized_captions]
-            padded_captions = [pad_sequences(caption, padding='post', maxlen=44) for caption in tokenized_captions]
-
-            np.save('y_shift_test.npy', padded_shift)
-            np.save('y_test.npy', padded_captions)
-            np.save('idx_test.npy', idx)
-            np.save('x_test.npy', normalized_feat)
+        np.save('id_' + mode + '.npy', video_id)
+        np.save('x_' + mode + '.npy', feat_data)
+        np.save('y_' + mode + '.npy', label_data)
+        if include == 'test':
+            video_id, feat_data, label_data = process_data(mode='test')
+            label_data = [tokenizer.texts_to_sequences(label) for label in label_data]
+            np.save('id_' + include + '.npy', video_id)
+            np.save('x_' + include + '.npy', feat_data)
+            np.save('y_' + include + '.npy', label_data)
     else:
-        idx, normalized_feat, sorted_label = get_data(data_dir, type, id_list)
-        np.save('idx_' + type + '.npy', idx)
-        np.save('x_' + type + '.npy', normalized_feat)
+        video_id, feat_data = process_data(mode='peer_review')
+        np.save('id_' + mode + '.npy', video_id)
+        np.save('x_' + mode + '.npy', feat_data)
 
 
 if __name__ == "__main__":
-
     import argparse
+    import os
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_dir', default='./MLDS_hw2_data/')
+    parser.add_argument('-data_dir', default='./MLDS_hw2_data/')
     parser.add_argument('-mode', default='train')
-    parser.add_argument('-include', default='train_data')
-    parser.add_argument('-use_list', default='none')
+    parser.add_argument('-include', default='test')
     args = parser.parse_args()
     print(args)
-
-    load_data(data_dir=args.data_dir, type=args.mode, include=args.include, id_list=args.use_list)
+    load_data(data_dir=args.data_dir, mode=args.mode, include=args.include)
