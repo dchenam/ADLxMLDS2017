dbd69a8e9d89274175796096d7ca17c10a8d9e2b
dchenam <dnnschen39@gmail.com>
2017-11-20 21:47:56 +0800

    major fixes
    Former-commit-id: c59a9f47142495a639479394ab9eb38a6530f0ba

--- hw2/hw2_seq2seq.sh
+++ hw2/hw2_seq2seq.sh
@@ -2,9 +2,9 @@
 data_path="$1"
 output_path="$2"
 peer_path="$3"
-wget 'https://www.dropbox.com/s/hy3kpi2cm3eg8fi/seq2seq.h5?dl=1' -O seq2seq.h5
-python preprocess.py -data_dir $data_path -include 'none'
-python seq2seq.py -output_dir $output_path -model_dir './seq2seq.h5'
+wget 'https://www.dropbox.com/s/wnh7b2etns2d8tq/best.h5?dl=1' -O seq2seq.h5
+python preprocess.py -data_dir $data_path
+python model_seq2seq.py -output_dir $output_path -model_dir './seq2seq.h5'
 
 python preprocess.py -data_dir $data_path -mode 'peer_review'
-python seq2seq.py -output_dir $peer_path -model_dir './seq2seq.h5'
+python model_seq2seq.py -mode 'peer_review' -output_dir $peer_path -model_dir './seq2seq.h5'
