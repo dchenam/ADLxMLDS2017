c03846e62eea7532493371fb7f47d20673196b3d
dchenam <dnnschen39@gmail.com>
2017-12-06 21:22:35 +0800

    tensorflow version
    Former-commit-id: 5c487ebddead474054f3bd786694cf8c7e550140

--- hw3/agent_dir/agent_pg.py
+++ hw3/agent_dir/agent_pg.py
@@ -4,13 +4,77 @@ import os
 import numpy as np
 from itertools import count
 
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-import torch.autograd as autograd
-from torch.autograd import Variable
+import tensorflow as tf
 
+class Policy:
+    def __init__(self, hidden_layer_size, learning_rate, checkpoints_dir):
+        self.learning_rate = learning_rate
+
+        self.sess = tf.InteractiveSession()
+
+        self.observations = tf.placeholder(tf.float32, [None, 6400])
+        # +1 for up, -1 for down
+        self.sampled_actions = tf.placeholder(tf.float32, [None, 1])
+        self.advantage = tf.placeholder(
+            tf.float32, [None, 1], name='advantage')
+
+        h = tf.layers.dense(
+            self.observations,
+            units=hidden_layer_size,
+            activation=tf.nn.relu,
+            kernel_initializer=tf.contrib.layers.xavier_initializer())
+
+        self.up_probability = tf.layers.dense(
+            h,
+            units=1,
+            activation=tf.sigmoid,
+            kernel_initializer=tf.contrib.layers.xavier_initializer())
+
+        self.loss = tf.losses.log_loss(
+            labels=self.sampled_actions,
+            predictions=self.up_probability,
+            weights=self.advantage)
+
+        optimizer = tf.train.AdamOptimizer(self.learning_rate)
+        self.train_op = optimizer.minimize(self.loss)
+
+        tf.global_variables_initializer().run()
+
+        self.saver = tf.train.Saver()
+        self.checkpoint_file = os.path.join(checkpoints_dir,
+                                            'policy_network.ckpt')
+
+    def load_checkpoint(self):
+        print("Loading checkpoint...")
+        self.saver.restore(self.sess, self.checkpoint_file)
+
+    def save_checkpoint(self):
+        print("Saving checkpoint...")
+        self.saver.save(self.sess, self.checkpoint_file)
+
+    def forward_pass(self, observations):
+        up_probability = self.sess.run(
+            self.up_probability,
+            feed_dict={self.observations: observations.reshape([1, -1])})
+        return up_probability
+
+    def train(self, state_action_reward_tuples):
+        print("Training with %d (state, action, reward) tuples" %
+              len(state_action_reward_tuples))
+
+        states, actions, rewards = zip(*state_action_reward_tuples)
+        states = np.vstack(states)
+        actions = np.vstack(actions)
+        rewards = np.vstack(rewards)
+
+        feed_dict = {
+            self.observations: states,
+            self.sampled_actions: actions,
+            self.advantage: rewards
+        }
+        self.sess.run(self.train_op, feed_dict)
+
+#From Andrej's Code
 def prepro(I):
     """ prepro 210x160x3 into 6400 """
     I = I[35:195]
@@ -20,18 +84,6 @@ def prepro(I):
     I[I != 0 ] = 1
     return I.astype(np.float).ravel()
 
-class Policy(nn.Module):
-    def __init__(self):
-        super(Policy,self).__init__()
-        self.affine1 = nn.Linear(6400, 200)
-        self.affine2 = nn.Linear(200, 3) # action 1 = stop, action 2 = move up, action 3 = move down
-        self.saved_actions = []
-        self.rewards = []
-    def forward(self, x):
-        x = F.relu(self.affine1(x))
-        action_scores = self.affine2(x)
-        return F.softmax(action_scores)
-
 class Agent_PG(Agent):
     def __init__(self, env, args):
         """
@@ -39,6 +91,7 @@ class Agent_PG(Agent):
         For example: building your model
         """
         super(Agent_PG,self).__init__(env)
+
         # hyperparameters
         self.hidden_units = 200  # number of hidden layer neurons
         self.batch_size = 5  # every how many episodes to do a param update?
@@ -47,72 +100,58 @@ class Agent_PG(Agent):
         self.decay_rate = 0.99  # decay factor for RMSProp leaky sum of grad^2
         self.epochs = 10000
         self.env = env
-        self.policy = Policy().cuda()
-        self.optimizer = optim.RMSprop(self.policy.parameters(), lr=self.learning_rate, weight_decay=self.decay_rate)
-        if args.resume:
-            if os.path.isfile('pg_params.pkl'):
-                print('loading trained model')
-                self.optimizer.load_state_dict(torch.load('pg_optim.pkl'))
-                self.policy.load_state_dict(torch.load('pg_params.pkl'))
-
-        if args.test_pg:
-            if os.path.isfile('pg_params.pkl'):
-                print('loading trained model')
-                self.policy.load_state_dict(torch.load('pg_params.pkl'))
+        if not os.path.exists('./checkpoints'):
+            os.makedirs('./checkpoints')
+        self.policy = Policy(hidden_layer_size=self.hidden_units, learning_rate=self.learning_rate, checkpoints_dir='./checkpoints/')
+        if args.resume or args.test_pg:
+            self.policy.load_checkpoint()
 
     def init_game_setting(self):
         """
-
         Testing function will call this function at the begining of new game
         Put anything you want to initialize if necessary
-
         """
         pass
 
-    def select_action(self, state):
-        state = torch.from_numpy(state).float().unsqueeze(0)
-        probs = self.policy(Variable(state).cuda())
-        action = probs.multinomial()
-        self.policy.saved_actions.append(action)
-        return action.data
-
-    def finish_episode(self):
+    def finish_episode(self, batch_state_action_reward_tuples):
+        states, actions, rewards = zip(*batch_state_action_reward_tuples)
         R = 0
-        rewards = []
+        discount_rewards = []
         #Discount Rewards
-        for r in self.policy.rewards[::-1]:
+        for r in rewards[::-1]:
             R = r + self.gamma * R
-            rewards.insert(0, R)
-        rewards = torch.Tensor(rewards)
+            discount_rewards.insert(0, R)
         #Normalize Rewards
-        rewards = (rewards - rewards.mean()) / (rewards.std() + np.finfo(np.float32).eps)
-        print(rewards)
-
-        #Approximate Gradient
-        for action, r in zip(self.policy.saved_actions, rewards):
-            action.reinforce(r)
-        self.optimizer.zero_grad()
-        autograd.backward(self.policy.saved_actions, [None for _ in self.policy.saved_actions])
-        self.optimizer.step()
-        #Clear Memory
-        del self.policy.rewards[:]
-        del self.policy.saved_actions[:]
+        discount_rewards = (discount_rewards - np.mean(discount_rewards)) / (np.std(discount_rewards) + np.finfo(np.float32).eps)
+        print(discount_rewards)
+        batch_state_action_reward_tuples = list(zip(states, actions, discount_rewards))
+        self.policy.train(batch_state_action_reward_tuples)
 
     def train(self):
         """
         Implement your training algorithm here
         """
         running_reward = None
-        reward_sum = 0
+        batch_state_action_reward_tuples = []
         for i_episode in count(1):
-            state = self.env.reset()
+            reward_sum = 0
+            observation = self.env.reset()
+            prev_state = prepro(observation)  # To compute difference frame
             for t in range(self.epochs):
-                state = prepro(state)
-                action = self.select_action(state)[0, 0]
-                action = action + 1
-                state, reward, done, _ = self.env.step(action)
+                state = prepro(observation)
+                state_delta = state - prev_state
+                prev_state = state
+                up_probability = self.policy.forward_pass(state_delta)[0]
+                if np.random.uniform() < up_probability:
+                    action = 2
+                else:
+                    action = 3
+                #Gym 2 is UP 3 is DOWN
+                observation, reward, done, _ = self.env.step(action)
                 reward_sum += reward
-                self.policy.rewards.append(reward)
+
+                #up_probability = self.policy.forward_pass(state_delta)[0]
+                batch_state_action_reward_tuples.append((state_delta, (action - 1), reward))
                 if done:
                     # tracking log
                     running_reward = reward_sum if running_reward is None else running_reward * 0.99 + reward_sum * 0.01
@@ -121,18 +160,18 @@ class Agent_PG(Agent):
                     break
 
                 if reward != 0:
-                    print('ep %d: game finished, reward: %f' % (i_episode, reward) + ('' if reward == -1 else ' !!!!!!!'))
+                    print('ep %d: game finished, reward: %f, steps: %d' % (i_episode, reward, t) + ('' if reward == -1 else ' !!!!!!!'))
 
             # use policy gradient update model weights
             if i_episode % self.batch_size == 0:
                 print('ep %d: policy network parameters updating...' % (i_episode))
-                self.finish_episode()
+                self.finish_episode(batch_state_action_reward_tuples)
+                batch_state_action_reward_tuples = []
 
             # Save model in every 50 episode
             if i_episode % 50 == 0:
                 print('ep %d: model saving...' % (i_episode))
-                torch.save(self.optimizer.state_dict(), 'pg_optim.pkl')
-                torch.save(self.policy.state_dict(), 'pg_params.pkl')
+                self.policy.save_checkpoint()
 
     def make_action(self, observation, test=True):
         """
@@ -146,8 +185,11 @@ class Agent_PG(Agent):
             action: int
                 the predicted action from trained model
         """
-        probs = self.policy(Variable(observation).cuda())
-        action = probs.multinomial()
-        self.policy.saved_actions.append(action)
+        state = prepro(observation)
+        up_probability = self.policy.forward_pass(state)[0]
+        if np.random.uniform() < up_probability:
+            action = 2
+        else:
+            action = 3
         return action.data
 
