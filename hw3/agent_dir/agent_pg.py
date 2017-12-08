04b964247547380ad3f8b15d3ffb87ff7c027d8e
dchenam <dnnschen39@gmail.com>
2017-12-08 17:12:31 +0800

    cleaned up code untested

--- hw3/agent_dir/agent_pg.py
+++ hw3/agent_dir/agent_pg.py
@@ -32,13 +32,8 @@ class Policy:
             xavier_l2 = tf.truncated_normal_initializer(mean=0, stddev=1. / np.sqrt(hidden_layer_size), dtype=tf.float32)
             self.tf_model['W2'] = tf.get_variable("W2", [hidden_layer_size, n_actions], initializer=xavier_l2)
 
-        # tf_discounted_epr = self.tf_discount_rewards(self.advantage)
-        # tf_mean, tf_variance = tf.nn.moments(tf_discounted_epr, [0], shift=None, name="reward_moments")
-        # tf_discounted_epr -= tf_mean
-        # tf_discounted_epr /= tf.sqrt(tf_variance + 1e-6)
-
         self.action_prob = self.forward_pass(self.observations)
-        loss = tf.nn.l2_loss(self.sampled_actions - self.action_prob)
+        loss = tf.nn.l2_loss(tf.one_hot(self.sampled_actions, n_actions) - self.action_prob)
         optimizer = tf.train.RMSPropOptimizer(learning_rate, decay=0.99)
         tf_grads = optimizer.compute_gradients(loss, var_list=tf.trainable_variables(), grad_loss=self.advantage)
         self.train_op = optimizer.apply_gradients(tf_grads)
@@ -49,7 +44,7 @@ class Policy:
         #     predictions=self.up_probability,
         #     weights=self.advantage)
         #
-        # #self.train_op = optimizer.apply_gradients(tf_grads)
+        #self.train_op = optimizer.apply_gradients(tf_grads)
         # self.train_op = optimizer.minimize(self.loss)
         self.sess = tf.InteractiveSession()
         tf.global_variables_initializer().run()
@@ -57,12 +52,6 @@ class Policy:
         self.checkpoint_path = checkpoints_dir
         self.checkpoint_file = os.path.join(checkpoints_dir,
                                             'policy_network.ckpt')
-
-    def tf_discount_rewards(self, tf_r):  # tf_r ~ [game_steps,1]
-        discount_f = lambda a, v: a * 0.99 + v;
-        tf_r_reverse = tf.scan(discount_f, tf.reverse(tf_r, [True, False]))
-        tf_discounted_r = tf.reverse(tf_r_reverse, [True, False])
-        return tf_discounted_r
 
     def forward_pass(self, observations):
         # action_prob = self.sess.run(
@@ -76,10 +65,6 @@ class Policy:
         return p
 
     def train(self, states, actions, rewards):
-        #print("Training with %d (state, action, reward) tuples" %
-        #      len(state_action_reward_tuples))
-
-        #states, actions, rewards = zip(*state_action_reward_tuples)
         states = np.vstack(states)
         actions = np.vstack(actions)
         rewards = np.vstack(rewards)
@@ -91,7 +76,6 @@ class Policy:
         }
         self.sess.run(self.train_op, feed_dict)
 
-#From Andrej's Code
 def prepro(I):
     """ prepro 210x160x3 into 6400 """
     I = I[35:195]
@@ -103,10 +87,6 @@ def prepro(I):
 
 class Agent_PG(Agent):
     def __init__(self, env, args):
-        """
-        Initialize every things you need here.
-        For example: building your model
-        """
         super(Agent_PG,self).__init__(env)
 
         # hyperparameters
@@ -125,11 +105,12 @@ class Agent_PG(Agent):
         self.states, self.actions, self.rewards = [], [], []
         self.reward_history = []
 
-
         if not os.path.exists('./checkpoints'):
             os.makedirs('./checkpoints')
+
         self.policy = Policy(hidden_layer_size=self.hidden_units, learning_rate=self.learning_rate,
                              n_actions=self.n_actions, checkpoints_dir='./checkpoints/')
+
         if args.resume or args.test_pg:
             self.load_checkpoint()
 
@@ -143,11 +124,13 @@ class Agent_PG(Agent):
     def discount_rewards(self, rewards):
         R = 0
         discount_rewards = []
+
         # Discount Rewards
         for r in rewards[::-1]:
             if r != 0: R = 0  # reset the sum, after someone scores a point
             R = r + self.gamma * R
             discount_rewards.insert(0, R)
+
         # Normalize Rewards
         discount_rewards -= np.mean(discount_rewards)
         discount_rewards /= np.std(discount_rewards) + np.finfo(np.float32).eps
@@ -155,7 +138,6 @@ class Agent_PG(Agent):
 
     def parameter_update(self):
         self.rewards = self.discount_rewards(self.rewards)
-        #batch_state_action_reward_tuples = list(zip(states, actions, rewards))
         self.policy.train(self.states, self.actions, self.rewards)
         del self.states[:]
         del self.actions[:]
@@ -179,8 +161,8 @@ class Agent_PG(Agent):
                 action_prob = self.policy.sess.run(self.policy.action_prob, feed)[0]
                 # action_prob = self.policy.forward_pass(state_delta)[0]
                 action = np.random.choice(self.n_actions, p=action_prob)
-                label = np.zeros_like(action_prob)
-                label[action] = 1
+                # label = np.zeros_like(action_prob)
+                # label[action] = 1
 
                 # Step Environment and Update Reward
                 state, reward, done, _ = self.env.step(action + 1)
@@ -188,24 +170,26 @@ class Agent_PG(Agent):
 
                 # Record Game History
                 self.states.append(state_delta)
-                self.actions.append(label)
+                self.actions.append(action)
                 self.rewards.append(reward)
                 round_n += 1
 
             # Logging
-            self.running_reward = reward_sum if self.running_reward is None else self.running_reward * 0.99 + reward_sum * 0.01
-            self.reward_history.append(self.running_reward)
+            if self.i_episode % 30 == 0:
+                self.running_reward = reward_sum if self.running_reward is None else self.running_reward * 0.99 + reward_sum * 0.01
+                self.reward_history.append(self.running_reward)
+
+            # Display Progress
             if self.i_episode % 10 == 0:
                 print(
                     'ep {}: reward: {}, mean reward: {:3f}'.format(self.i_episode, reward_sum, self.running_reward))
             else:
                 print('\tep {}: finished after {} rounds, reward: {}'.format(self.i_episode, round_n, reward_sum))
 
-
             # Update Parameter
             if self.i_episode % self.batch_size == 0:
-                #print('ep %d: policy network parameters updating...' % (self.i_episode))
                 self.parameter_update()
+
             # Save model in every 50 episode
             if self.i_episode % 50 == 0:
                 print('ep %d: model saving...' % (self.i_episode))
@@ -218,8 +202,11 @@ class Agent_PG(Agent):
         load_path = ckpt.model_checkpoint_path
         self.policy.saver.restore(self.policy.sess, load_path)
         self.policy.saver = tf.train.Saver(tf.global_variables())
-        self.reward_history = pickle.load(open('./reward_history.p', 'rb'))
-        self.i_episode = int(load_path.split('-')[-1])
+        try:
+            self.reward_history = pickle.load(open('./reward_history.p', 'rb'))
+            self.i_episode = int(load_path.split('-')[-1])
+        except:
+            pass
 
     def save_checkpoint(self):
         print("Saving checkpoint...")
@@ -244,8 +231,8 @@ class Agent_PG(Agent):
         self.prev_state = state
 
         # Sample Stochastic Policy
-        action_prob = self.policy.forward_pass(state_delta)[0]
-        print(action_prob)
+        feed = {self.policy.observations: state_delta.reshape([1, -1])}
+        action_prob = self.policy.sess.run(self.policy.action_prob, feed)[0]
         action = np.random.choice(self.n_actions, p=action_prob)
         return action + 1
 
