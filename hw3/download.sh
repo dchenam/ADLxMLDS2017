200bd571695be9b8f6efefc6b658ae5ef6bdef1f
dchenam <dnnschen39@gmail.com>
2017-12-15 17:06:17 +0800

    fixed bug
    Former-commit-id: ee05ba6b13dd2dbeeb5283bc5367e9abe7621698

--- hw3/download.sh
+++ hw3/download.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 wget 'https://www.dropbox.com/s/gyoubzz9zi2s2vn/Vanilla_DQN.zip?dl=1' -O Vanilla_DQN.zip
 wget 'https://www.dropbox.com/s/ockjz89j37e0xcu/REINFORCE.zip?dl=1' -O REINFORCE.zip
+mkdir saved
 unzip REINFORCE.zip -d saved
 unzip Vanilla_DQN.zip -d saved
 rm REINFORCE.zip
