7d4d5a36be55b632e269e5db55b7b9608e48a9bb
dchenam <dnnschen39@gmail.com>
2018-01-07 23:08:14 +0800

    fixed directory in bash

--- hw4/run.sh
+++ hw4/run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 testing_text="$1"
 wget 'https://www.dropbox.com/s/jqqok11p8c4vzqx/checkpoints.zip?dl=1' -O checkpoints.zip
+mkdir experiments/GAN-CLS/
 unzip checkpoints.zip -d experiments/GAN-CLS/
 rm checkpoints.zip
 python3 generate.py --testing $testing_text
